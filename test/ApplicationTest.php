bf3445b747baab48e870d2fb483c3fe0dc3bab09
trick <trick.developer@gmail.com>
2015-02-12 15:23:40 -0500

    Fix unit test

--- test/ApplicationTest.php
+++ test/ApplicationTest.php
@@ -13,7 +13,7 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
     if (file_exists("/home/travis/build/patinthehat/new-php-project/new-php-project.php"))
       $fn = "/home/travis/build/patinthehat/new-php-project/new-php-project.php";
     
-    chdir(realpath($fn));
+    chdir(dirname(realpath($fn)));
     if (file_exists($fn)) {
       $cmd = "$fn $projectName";
       $this->assertFalse(project_exists($projectName));
