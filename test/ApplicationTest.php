b5059a9490508519d1672b1899b49d6a3ba5ccb4
trick <trick.developer@gmail.com>
2015-02-12 15:16:41 -0500

    Fix unit test

--- test/ApplicationTest.php
+++ test/ApplicationTest.php
@@ -7,7 +7,7 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
   public function testApplication() 
   {
     $projectName = "MyProject".mt_rand(10000, 99999);
-    chdir(dirname(__FILE__)."/..");
+    //chdir(dirname(__FILE__)."/..");
     $cmd = "new-php-project.php $projectName";
     $this->assertFalse(project_exists($projectName));
     system($cmd);
